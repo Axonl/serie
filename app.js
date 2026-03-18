49d3dc78402a548fbc148d26d57320759c9f3bf2
muunuo <benkforland@gmail.com>
2026-03-18 09:34:22 +0100

    fikset småfeil med creat if not exist
    fikset småfeil med creat if not exist

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const pool = mysql.createPool({ // trengs grunnet bruk av Maria.db.
   user: 'root',
   password: 'root',
   database: 'serie',
-  connectionLimit: 5
+  connectionLimit: 5,
+  multipleStatements: true //tillater flere statments samtidig (ai)
 });
 
-pool.exec(`
+pool.execute(`
 CREATE TABLE IF NOT EXISTS bruker (
-    bruker_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    bruker_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     brukernavn TEXT UNIQUE,
     passord TEXT,
     kallenavn TEXT,
@@ -23,34 +24,33 @@ CREATE TABLE IF NOT EXISTS bruker (
 );
 
 CREATE TABLE IF NOT EXISTS serie (
-    serie_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    serie_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     navn TEXT,
     beskrivelse TEXT,
     utgivelses_aar TEXT,
-    andmeldelse TEXT,
+    anmeldelse TEXT,
     bilde BLOB
 );
 
 CREATE TABLE IF NOT EXISTS anbefaling (
-    anbefaling_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    anbefaling_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     kommentar TEXT,
     er_godtatt INTEGER,
     serie_id INTEGER,
     mottaker_id INTEGER,
     sender_id INTEGER,
-    kommentar TEXT,
     FOREIGN KEY (serie_id) REFERENCES serie(serie_id),
     FOREIGN KEY (mottaker_id) REFERENCES bruker(bruker_id),
     FOREIGN KEY (sender_id) REFERENCES bruker(bruker_id)
 );
 
 CREATE TABLE IF NOT EXISTS status_serie (
-    status_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    status_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     status TEXT,
     bruker_id INTEGER,
     serie_id INTEGER,
     FOREIGN KEY (serie_id) REFERENCES serie(serie_id),
-    FOREIGN KEY (bruker_id) REFERENCES bruker(bruker_id),
+    FOREIGN KEY (bruker_id) REFERENCES bruker(bruker_id)
 );
 `);
 
